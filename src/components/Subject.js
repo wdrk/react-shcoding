99bd77515dd50d282053967924aaf941179fd980
WDRK <lllrrrlrssr@gmail.com>
2021-04-20 16:54:31 +0900

    컴포넌트 파일 add 안돼서 추가함

---
+++ src/components/Subject.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
