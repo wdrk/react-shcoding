5abb1399f9b7adf605b96be7eba1e28abb1c0997
WDRK <lllrrrlrssr@gmail.com>
2021-04-21 15:15:32 +0900

    Subject와 Content에 state를 적용함

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import "./App.css";
-import React, { Component } from "react";
-import TOC from "./components/TOC";
-import Subject from "./components/Subject";
-import Content from "./components/Content";
+import './App.css';
+import React, { Component } from 'react';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {
+        title: 'WEB',
+        sub: 'World Wide Web!',
+      },
+      content: {
+        title: 'HTML',
+        desc: 'HTML is HyperText Markup Language.',
+      },
+    };
+  }
   render() {
     return (
-      <div className='App'>
-        <Subject title='WEB' sub='World Wide Web!'></Subject>
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
         <TOC></TOC>
         <Content
-          title='HTML'
-          desc='HTML is HyperText Markup Language.'
+          title={this.state.content.title}
+          desc={this.state.content.desc}
         ></Content>
       </div>
     );
