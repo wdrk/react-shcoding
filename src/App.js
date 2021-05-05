2689c258966ad9dca07d2561338d39b28ee24596
WDRK <lllrrrlrssr@gmail.com>
2021-05-05 20:38:20 +0900

    MyName.js를 만들어서 MyName 컴포넌트에 간단한 props를 넘기는 코드

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import './App.css';
 import React, { Component } from 'react';
-import MyComponent from './myComponent';
+import MyName from './MyName.js';
 
 class App extends Component {
-  state = {
-    counter: 1,
-  };
-  constructor(props) {
-    super(props);
-    console.log('constructor');
-  }
-  componentDidMount() {
-    console.log('componentDidMount');
-  }
-  handleClick = () => {
-    this.setState({
-      counter: this.state.counter + 1,
-    });
-  };
   render() {
-    return (
-      <div>
-        {/* JSX 내부에서 주석을 쓰려면 멀티라인 주석을 중괄호로 감싸면 된다*/}
-        <h1
-          something="dsalfkjdf" // 컴포넌트 안에 이렇게 주석을 쓸 수 있다
-        >
-          리액트
-        </h1>
-      </div>
-    );
+    return <MyName name="리액트"></MyName>;
   }
 }
 
