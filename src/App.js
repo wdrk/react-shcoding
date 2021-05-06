2ba4f4ab3335d68d238dfc02dc905248826f5301
WDRK <lllrrrlrssr@gmail.com>
2021-05-06 22:47:32 +0900

    componentDidCatch() 는 에러가 발생하는 컴포넌트의 상위 컴포넌트에서 설정한다

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 import Counter from './Counter.js';
 
 class App extends Component {
+  state = {
+    error: false,
+  };
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+  }
   render() {
+    if (this.state.error) {
+      return <div>에러가 났어요</div>;
+    }
     return <Counter></Counter>;
   }
 }
