0a2195e56a6269f96e5fd73b541d433426956176
WDRK <lllrrrlrssr@gmail.com>
2021-05-22 22:58:18 +0900

    Refactor: 추가 직전 커밋할때 누락한 코드
    App.js에서 작업하고 staging을 안한 코드를 추가함

--- src/App.js
+++ src/App.js
@@ -2,17 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
-
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
+import Subject from "./components/Subject";
 
 class App extends Component {
   render() {
