5bba9d1784e641a4d9cae29783ebbb0cc3356483
WDRK <lllrrrlrssr@gmail.com>
2021-05-23 15:06:32 +0900

    Refactor: 수정 Subject props가 변수를 읽도록
    Subject의 props가 여러가지 state값을 받을 수 있도록 하기 위해서
    분기문을 통해 두가지 state 중에 하나의 값을 받는 변수를 만들고
    그 변수값을 Subject의 props에 대입하는 구조로 수정

--- src/App.js
+++ src/App.js
@@ -3,23 +3,36 @@ import "./App.css";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "read",
       subject: {
         title: "WEB",
         sub: "World Wide Web!",
       },
+      welcome: {
+        title: "Welcome",
+        desc: "Hello, React!",
+      },
       contents: [
-        { id: 1, title: "HTML", desc: "HTML is HyperText" },
+        { id: 1, title: "HTML", desc: "HTML is for information" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
         { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
       ],
     };
   }
   render() {
+    let _title = null;
+    let _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject
@@ -27,10 +40,7 @@ class App extends Component {
           sub={this.state.subject.sub}
         ></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language."
-        ></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
