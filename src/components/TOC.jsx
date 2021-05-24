c0937e12ae96ff9131d5a90786063d80b4efc379
WDRK <lllrrrlrssr@gmail.com>
2021-05-24 19:07:27 +0900

    Feat: 추가 TOC.jsx에 shouldComponentUpdate() 구간
    TOC의 내용이 변하지 않으면 TOC를 render()하지 않음으로서 성능 향상을 위해

--- src/components/TOC.jsx
+++ src/components/TOC.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class TOC extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    if (newProps.data === this.props.data) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   render() {
     const lists = [];
     const data = this.props.data;
